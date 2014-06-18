9c0d4c9db1ca531fcca60400abd31b2b2c39100e
frank guo <pkushiqiang@gmail.com>
2014-06-18 16:42:02 -0500

    modify the cron job script

--- python/src/cronjob/crawindeed.sh
+++ python/src/cronjob/crawindeed.sh
@@ -1,9 +1,29 @@
 datestr=`date +%Y-%m-%d`
 logfile="./log/"$datestr"_indeed.log"
+LIST_COLL_NAME="daily_job_list_"$datestr
+INFO_COLL_NAME="daily_job_info_"$datestr
 #logfile=$datestr"_indeed.log"
 echo "craw log to: "$logfile
+
+
+#python cronjob.py
 python cronjob.py > $logfile 2>&1
+
+exec 1>> $logfile
+exec 2>> $logfile
+echo "python result is:"$?
 #echo "test test test " > $logfile
 #python raiseexception.py > $logfile 2>&1
 
- 
+mongoexport -d jobaly_daily -c $LIST_COLL_NAME -o ./data/$LIST_COLL_NAME.json  
+mongoexport -d jobaly_daily -c $INFO_COLL_NAME -o ./data/$INFO_COLL_NAME.json  
+
+#tar -cvzf ./data/$LIST_COLL_NAME.tar.gz  ./data/$LIST_COLL_NAME.json --remove-files
+#tar -cvzf ./data/$INFO_COLL_NAME.tar.gz  ./data/$INFO_COLL_NAME.json --remove-files
+
+tar -cvzf ./data/$LIST_COLL_NAME.tar.gz  ./data/$LIST_COLL_NAME.json  
+tar -cvzf ./data/$INFO_COLL_NAME.tar.gz  ./data/$INFO_COLL_NAME.json  
+
+mv ./data/$LIST_COLL_NAME.tar.gz    /home/frank/Dropbox/jobaly_daily
+mv ./data/$INFO_COLL_NAME.tar.gz    /home/frank/Dropbox/jobaly_daily
+echo "cron job completed"
--- python/src/cronjob/cronjob.py
+++ python/src/cronjob/cronjob.py
@@ -1,41 +1,60 @@
 import sys
 sys.path.append("..")
 from jobaly.db.dbclient import DbClient 
-from jobinfo_daily import *
-from indeedcrawler import *
+import jobaly.utils
+import jobinfo_daily 
+import indeedcrawler 
+import datetime
 import time
+import os
+
+def getJobList( dbClient, listCollectionName ):
+     lang_names = jobaly.utils.loadArrayFromFile("lang_list.txt")  
+     cities = jobaly.utils.loadArrayFromFile("loc_list.txt")  
+     
+  #   lang_names = jobaly.utils.loadArrayFromFile("test_lang_list.txt")  
+  #   cities = jobaly.utils.loadArrayFromFile("test_loc_list.txt") 
+    
+     listCollection = dbClient.getCollection(listCollectionName)      
+     start_time = time.time()
+     print "---- start get job list ----"
+   #  getJobList(listCollectionName) 
+     days = 1
+     indeedcrawler.crawlIndeed(listCollection, lang_names, cities, days ) 
+     t =  time.time() - start_time 
+     print "---- finish get job list, use %s seconds  ----" %t
+     
+def getJobInfo(dbClient, listCollectionName, infoCollectionName ):
+     print 
+     print
+     listCollection = dbClient.getCollection(listCollectionName)   
+     infoCollection = dbClient.getCollection(infoCollectionName)
+     start_time = time.time()     
+     print "---- start get job info ----"
+     jobinfo_daily.getJobInfo(dbClient, listCollection, infoCollection)
+     t =  time.time() - start_time
+     print "---- finish get job info, use %s seconds  ----" %t
 
 def main():
-     days = 1
+     t =  datetime.datetime.now()
+     print "start at:" , t  
      today = datetime.date.today()    
      listCollectionName = "daily_job_list_"+str(today)
      print "list collection name:", listCollectionName
      infoCollectionName = "daily_job_info_"+str(today)
      print "info collection name:", infoCollectionName
      
-     lang_names = jobaly.utils.loadArrayFromFile("lang_list.txt")  
-     cities = jobaly.utils.loadArrayFromFile("loc_list.txt")  
-  
      dbClient = DbClient('localhost', 27017, "jobaly_daily")
-     listCollection = dbClient.getCollection(listCollectionName)  
-    
-     start_time = time.time()
-     print "---- start get job list ----"
-   #  getJobList(listCollectionName) 
-     crawlIndeed(listCollection, lang_names, cities,days ) 
-     t =  time.time() - start_time 
-     print "---- finish get job list, use %s seconds  ----" %t
-     
-     print 
-     print
-     infoCollection = dbClient.getCollection(infoCollectionName)
-     start_time = time.time()     
-     print "---- start get job info ----"
-     getJobInfo(dbClient, listCollection, infoCollection)
-     t =  time.time() - start_time
-     print "---- finish get job info, use %s seconds  ----" %t
-    
-     
+     getJobList( dbClient, listCollectionName )
+     getJobInfo( dbClient, listCollectionName, infoCollectionName )      
+  
+   
+  #   os.environ["LIST_COLL_NAME"] = listCollectionName
+  #   os.environ["INFO_COLL_NAME"] = infoCollectionName
+  #   os.system("bash")     
+ 
+     t =   datetime.datetime.now()
+     print "end at:" , t 
     
 if __name__ == "__main__": 
     main()
