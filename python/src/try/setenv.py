c7e7fe917d133d1d734ce8ccf8d958136733bf04
frank guo <pkushiqiang@gmail.com>
2014-06-18 15:29:06 -0500

    add try of set environment variable

--- python/src/try/setenv.py
+++ python/src/try/setenv.py
@@ -1,4 +1,11 @@
 import os
-print os.environ["PATH"]
-os.environ["MY_VAR"] = "99999"
+import datetime
+#print os.environ["PATH"]
+today = str(datetime.date.today())  
+#os.environ["COLL_DATE"] = today
+listCollectionName = "daily_job_list_"+str(today)     
+infoCollectionName = "daily_job_info_"+str(today)
+os.environ["LIST_COLL_NAME"] = listCollectionName
+os.environ["INFO_COLL_NAME"] = infoCollectionName
+
 os.system("bash")
