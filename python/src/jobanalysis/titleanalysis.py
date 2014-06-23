6c8e7a257b4558fbe4551de62677d7161d075e81
Frank Guo <pkushiqiang@gmail.com>
2014-06-23 01:16:25 -0500

    add filters

--- python/src/jobanalysis/titleanalysis.py
+++ python/src/jobanalysis/titleanalysis.py
@@ -7,8 +7,8 @@ Created on Sun Jun 22 14:14:13 2014
 import sys
 sys.path.append("..")
 import jobaly.utils
-import re
 import json
+import tokenfilter
 
 
 def dumpToText(listObj, fileName):
@@ -27,121 +27,30 @@ def dumpTwo(listObj, fileName):
     jsonFileName = fileName+".json"
     dumpToText(listObj, txtFileName)
     dumpToJson(listObj, jsonFileName)
-    
-def orContains(string, containList):
-    
-    for item in containList:
-        item = item.lower()         
-        if contains(string,item) :
-                return True         
-    return False
-    
-def seqContains(string, containList):
-     
-    for item in containList:        
-         i = string.find(item) 
-         if i == -1 :
-             return False
-         else :
-             string = string[i+len(item):]
-              #   print string
-             
-    return True   
-    
-def orFind(string, containStr):
-     orList = containStr.split("|")   
-     minIndex = len(string)
-     for item in orList:
-         i = string.find(item)
-         if i != -1 and i < minIndex:
-             minIndex = i
-             
-     if minIndex == len(string):
-         return -1
-     else :
-         return   minIndex
-    
 
-def contains(string, containStr):
-    if ("," not in containStr) and ("|" not in containStr):
-        return  containStr in string 
-    elif  "," in containStr:
-        seqList = containStr.split(",")
-        for item in seqList:        
-             i = orFind(string,item) 
-             if i == -1 :
-                 return False
-             else :
-                 string = string[i+len(item):]
-                  #   print string
-             
-        return True   
-    
-    
-def filterByContains(titleList, containList):
-    resultList=[]
-    leftList=[]    
-    
-    for item in titleList:
-        #  print item["_id"].encode("GBK", "ignore")
-        if  orContains( item["_id"].lower(), containList ) :
-            print item["_id"].encode("GBK", "ignore")
-            resultList.append(item)
-        else :
-            leftList.append(item)
-            
-    return ( resultList, leftList )
-    
-def filterByRegex(titleList):
-    resultList=[]
-    leftList=[]
-    pattern = re.compile('(.*Software Engineer)|(.*Software Developer)',re.IGNORECASE)
-    
-    for item in titleList:
-        #  print item["_id"].encode("GBK", "ignore")
-        if  pattern.match( item["_id"] ) :
-            print item["_id"].encode("GBK", "ignore")
-            resultList.append(item)
-        else :
-            leftList.append(item)
-            
-    return ( resultList, leftList )
+def filterCat(titleList , catName):
+    filterFile = "filters//"+catName+"_filter.txt"
+    filterList = jobaly.utils.loadArrayFromFile(filterFile)  
+    resultList, newleftList = tokenfilter.filterByContains(titleList, filterList )
+    dumpTwo(resultList, "titles//"+catName) 
+    return newleftList
 
-def filterSDE(titleList ):
-    sedlist = []
-    leftlist = []
-    for item in titleList:
-        #  print item["_id"].encode("GBK", "ignore")
-        if  item["_id"].find("Software Engineer")!=-1 or \
-            item["_id"].find("Software Developer")!=-1  :
-            print item["_id"].encode("GBK", "ignore")
-            sedlist.append(item)
-        else :
-            leftlist.append(item)
-    
-    return (sedlist, leftlist )
-    
-def test_orFind():
-    print "result =" , str(orFind("aaeer ew", "ee|aa"))
-    print "result =" , str(orFind("saaeer ew", "ee|aa"))
-    print "result =" , str(orFind("saeder ew", "ee|aa|ewr"))
-    print "result =" , str(orFind("saeder ew", "ee|aa|e"))
-    print "result =" , str(orFind("saeder ew", "eewe"))
-    print "result =" , str(orFind("saeder ew", "e"))
-  
-def test_seqContains():
-    
-    print "result =" , str(seqContains("aaaa  bb", ["bb", "CC"]))
-    print "result =" , str(seqContains("aaaa  bb CC", ["bb", "CC"]))
-    print "result =" , str(seqContains("123456bb CC", ["1","34", "CC"]))
- 
 def main():   
     
     titleList = json.loads(open('titles//titleList.json').read())
     # print type(titleList)
-    devFilter = jobaly.utils.loadArrayFromFile("devfilter.txt")  
-    resultList, leftList = filterByContains(titleList, devFilter )
-    dumpTwo(resultList, "titles//softeng")  
+    devFilter = jobaly.utils.loadArrayFromFile("filters//dev_filter.txt")  
+    resultList, leftList = tokenfilter.filterByContains(titleList, devFilter )
+    dumpTwo(resultList, "titles//softeng")     
+    
+    leftList =   filterCat(leftList, "dba")
+    leftList =   filterCat(leftList, "qa")
+    leftList =   filterCat(leftList, "data")
+    leftList =   filterCat(leftList, "programman")
+    leftList =   filterCat(leftList, "productman")
+    leftList =   filterCat(leftList, "support")
+    
+   
     dumpTwo(leftList, "titles//lefttitle")  
     
 if __name__ == "__main__": 
---
+++ python/src/jobanalysis/tokenfilter.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 22 22:25:07 2014
+
+@author: dlmu__000
+"""
+import re
+    
+def orContains(string, containList):
+    
+    for item in containList:
+        item = item.lower()         
+        if contains(string,item) :
+                return True         
+    return False
+    
+def seqContains(string, containList):
+     
+    for item in containList:        
+         i = string.find(item) 
+         if i == -1 :
+             return False
+         else :
+             string = string[i+len(item):]
+              #   print string
+             
+    return True   
+    
+def orFind(string, containStr):
+     orList = containStr.split("|")   
+     minIndex = len(string)+10
+     for item in orList:
+         i = string.find(item)
+         if i != -1 and i+len(item) < minIndex:
+             minIndex = i+len(item)
+             
+     if minIndex == len(string)+10:
+         return -1
+     else :
+         return   minIndex
+    
+
+def contains(string, containStr):
+    if ("," not in containStr) and ("|" not in containStr):
+        return  string.find(containStr) != -1
+    else:        
+        seqList = containStr.split(",")
+     #   print seqList
+        for item in seqList:        
+             i = orFind(string,item) 
+             if i == -1 :
+                 return False
+             else :
+                 string = string[i:]
+         #        print string             
+        return True   
+    
+    
+def filterByContains(titleList, containList):
+    resultList=[]
+    leftList=[]    
+    
+    for item in titleList:
+        #  print item["_id"].encode("GBK", "ignore")
+        if  orContains( item["_id"].lower(), containList ) :
+            print item["_id"].encode("GBK", "ignore")
+            resultList.append(item)
+        else :
+            leftList.append(item)
+            
+    return ( resultList, leftList )
+    
+def filterByRegex(titleList):
+    resultList=[]
+    leftList=[]
+    pattern = re.compile('(.*Software Engineer)|(.*Software Developer)',re.IGNORECASE)
+    
+    for item in titleList:
+        #  print item["_id"].encode("GBK", "ignore")
+        if  pattern.match( item["_id"] ) :
+            print item["_id"].encode("GBK", "ignore")
+            resultList.append(item)
+        else :
+            leftList.append(item)
+            
+    return ( resultList, leftList )
+    
+    
+
+    
+def test_orFind():
+    print "result =" , str(orFind("aaeer ew", "ee|aa"))
+    print "result =" , str(orFind("saaeer ew", "ee|aa"))
+    print "result =" , str(orFind("saeder ew", "ee|aa|ewr"))
+    print "result =" , str(orFind("saeder ew", "ee|aa|e"))
+    print "result =" , str(orFind("saeder ew", "eewe"))
+    print "result =" , str(orFind("saeder ew", "e"))
+  
+def test_seqContains():
+    
+    print "result =" , str(seqContains("aaaa  bb", ["bb", "CC"]))
+    print "result =" , str(seqContains("aaaa  bb CC", ["bb", "CC"]))
+    print "result =" , str(seqContains("123456bb CC", ["1","34", "CC"]))
+
+def test_contains():
+  #   print contains("aabb","cc|aa")
+  #   print contains("Front End Developer for a Hot Fashion Startup from Techstars", "Front End|Front")
+  #   print contains("Java Developer- mid-level", "Java|Python|Ruby,Developer|Engineer|Architect|Development" )     
+  #   print contains("Java Developer- mid-level", "Java|Python|Ruby,Developer" )     
+      print contains( "Java/J2EE Developer", "Java|Python|Ruby,Developer" )   
+def main():   
+    test_contains() 
+    
+if __name__ == "__main__": 
+    main()
