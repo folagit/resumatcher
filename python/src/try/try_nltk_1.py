9e467dda30fed7db9fed15a8f2db2c11482b953f
Frank Guo <pkushiqiang@gmail.com>
2014-07-22 23:13:08 -0500

    update try

---
+++ python/src/try/try_nltk_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 22 21:54:05 2014
+
+@author: dlmu__000
+"""
+import nltk
+
+sent="BA/BS degree or equivalent practical experience"
+
+tokens = nltk.word_tokenize(sent)
+print tokens
