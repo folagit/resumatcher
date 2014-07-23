171c046e216153388c2a6be8761e2a9eb6f0b88d
Frank Guo <pkushiqiang@gmail.com>
2014-07-22 23:13:32 -0500

    add module template

---
+++ python/temp_main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 22 23:12:16 2014
+
+@author: dlmu__000
+"""
+
+
+def main():
+    print "----- good part 1 ----"
+    
+
+if __name__ == '__main__':
+    main()
