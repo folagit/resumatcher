47ed2cc3a781b993d26ef639a27d8af432ad98ef
Frank Guo <pkushiqiang@gmail.com>
2014-07-29 00:09:58 -0500

    change try folders

---
+++ python/src/try/outerlibs/try_prettytable.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 29 00:05:16 2014
+
+@author: dlmu__000
+"""
+
+from prettytable import PrettyTable
+x = PrettyTable()
+
+x.add_row(["Darwin", 112, 120900, 1714.7])
+x.add_row(["Hobart", 1357, 205556, 619.5])
+
+print x
---
+++ python/src/try/outerlibs/try_tabulate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 28 19:11:54 2014
+
+@author: dlmu__000
+"""
+
+from tabulate import tabulate
+print tabulate([["spam", 1], ["eggs", 42]])
