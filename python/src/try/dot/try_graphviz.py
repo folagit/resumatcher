a25c7c2654b66e2c74682f55abfc0c606776d3d2
Frank Guo <pkushiqiang@gmail.com>
2014-08-05 22:05:24 -0500

    update test

--- python/src/try/dot/try_graphviz.py
+++ python/src/try/dot/try_graphviz.py
@@ -6,12 +6,14 @@ Created on Tue Aug 05 17:15:55 2014
 """
 
 from graphviz import Digraph
+import os
 
 def createPng(fileName):
     dotfile = 'C:\\graphviz-2.38\\release\\bin\\dot.exe'
     cmd=[dotfile, '-Tpng',  fileName+'.gv', "-o", fileName+".png"]
     #cmd=[dotfile, '-Tpdf', '-O', 'round-table.gv']
     returncode = subprocess.Popen(cmd).wait()
+    os.startfile(os.path.normpath(fileName+".png"))
     print "returncode=", returncode
     
 def createDotFile(dot,fileName):
@@ -30,5 +32,26 @@ def test1():
     dot.edge('B', 'L', constraint='false')
     createDotFile(dot, "test1")
     # print(dot.source)
+
+def test2():
+    dot = Digraph(comment='The Round Table', \
+        node_attr={"shape":"circle"})
+        
+    dot.append("rankdir=LR;")
+    dot.append('size="8,5"')
+        
+    dot.node('A', "", {"shape":"doublecircle"} )
+    dot.node('B' ,"" )
+    dot.node('L', ""  )
+    dot.node('S', "", {"shape":"point"} )
+     
+    dot.edge('S', 'A', "e")
+    dot.edge('A', 'B', "a")
+    dot.edge('B', 'L', "b")
+    dot.edge('L', 'L', "l")
+   # dot.edge('B', 'L', constraint='false')
+    createDotFile(dot, "outfiles\\test2")
+
  
-test1()
+    
+test2()
