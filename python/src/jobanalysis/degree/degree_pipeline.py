c3aeb5419288fde910ed680b228273014b22396b
Frank Guo <pkushiqiang@gmail.com>
2014-08-12 13:16:20 -0500

    add module degree_pipeline

---
+++ python/src/jobanalysis/degree/degree_pipeline.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 12 12:29:07 2014
+
+@author: dlmu__000
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 25 20:31:27 2014
+
+@author: dlmu__000
+
+"""
+import os, sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from data.jobsentence import JobSentence
+from jobaly.match.matcher  import *
+
+from  data import datautils
+
+from data.jobsentence import JobSentence
+from degreelabeler import *
+
+
+def onlyDegreeLevel(result):
+ #   print "result=",result
+    newresult = []
+    for item in result:
+  #      print "item=",item
+        if item != [] and \
+          labelGrammer.ontoDict.has_key(item):
+            newresult.append(item)
+    return newresult
+                
+
+matcher1 = LabelMatcher("DE_LEVEL")
+matcher2 = LabelMatcher("DEGREE")
+degreeSeq1 = SeqMatcher([matcher1,matcher2], outfun=onlyDegreeLevel)
+
+matcher4 = StarMatcher(LabelMatcher([",","DE_LEVEL"]))
+matcher5 = QuestionMatcher(LabelMatcher(["OR","DE_LEVEL"]))
+degreeSeq2 = SeqMatcher([matcher1,matcher4, matcher5, matcher2], outfun=onlyDegreeLevel)
+   
+
+matcher10 = LabelMatcher( [ "IN" , "MAJOR" ])
+matcher11 = StarMatcher( LabelMatcher( [",", "MAJOR"] ) )
+matcher12 = LabelMatcher( [ "OR" , "MAJOR" ])
+matcher13 = SeqMatcher([matcher10, matcher11, matcher12])
+ 
+def getlabeledArray(sent ):
+    degreeSent = JobSentence(sent.split())
+    labelGrammer.labelSentence(degreeSent)
+    print degreeSent.printSentenct()  
+    labeledArray = degreeSent.getLabeledArray(labelGrammer.ontoDict)
+    print degreeSent.printLabeledArray() 
+    return labeledArray
+ 
+def getDegreeLevel( matcher , sent ):
+    labeledArray = getlabeledArray( sent )
+    i = matcher.findMatching(labeledArray)
+    return i, matcher.output()
+
+    
+def labelDegreeSet(data_set_name, outfileName):
+    labelGrammer =  createDegreeGrammar()      
+    data = datautils.loadJson(data_set_name)
+    matcher =  degreeSeq2
+    f = open(outfileName, "w")
+    total = 0
+    m = 0
+    for item in data:
+    #    print item
+        sent = item[2]       
+        f.write( sent +"\n\n" )
+        degreeSent = JobSentence(sent.split())
+        labelGrammer.labelSentence(degreeSent)
+     #   print degreeSent.printSentenct()  
+    #    f.write( degreeSent.printSentenct().get_string() +"\n\n" )
+        labeledArray = degreeSent.getLabeledArray(labelGrammer.ontoDict)
+    #    print degreeSent.printLabeledArray() 
+        f.write( degreeSent.printLabeledArray().get_string() +"\n\n" )
+        i = matcher.findMatching(labeledArray)
+        print i, matcher.output()
+        f.write( str(i) + "   " + str(matcher.output()) +"\n\n" )
+        total += 1
+        if i != -1 :
+            m+=1
+     
+    print "match=", m, "  total=", total, "  radio=", float(m)/total
+
+def main(): 
+     
+   data_set_name = "matching_muldegree_3"  
+   target_set_name = "degree_3"
+   outfileName = "degree_3_label.txt"
+   
+  
+   target_set_name = "output\\degree_1"   
+   outfileName = "output\\degree_1_layer2.txt"  
+
+   labelDegreeSet(target_set_name,outfileName) 
+   
+if __name__ == "__main__": 
+    main() 
