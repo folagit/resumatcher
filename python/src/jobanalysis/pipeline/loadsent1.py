0857d71420e05e4b1a5ac2b721b6d62cb62a8e2f
Frank Guo <pkushiqiang@gmail.com>
2014-09-02 14:52:20 -0500

    update

--- python/src/jobanalysis/pipeline/loadsent1.py
+++ python/src/jobanalysis/pipeline/loadsent1.py
@@ -10,13 +10,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from jobaly.db.dbclient import DbClient
+from jobaly.ontology.ontologylib import OntologyLib
 from jobdescparser import JobDescParser
 from nltk.tokenize import word_tokenize
 from data.datautils import dumpTwo
 
-def processColl(collection):
-    allSents.extend(sents)  
-    
 
 def getJavaScipt(): 
      srcBbClient = DbClient('localhost', 27017, "jobaly_daily_test")
@@ -65,9 +63,43 @@ def testGetSentenceByTerm(term):
      outputPath = '..\skill\output\\' + term
      getSentenceByTerm(collection, term, outputPath)
      
+
+def getSentsByOntology():
+     owlfile = "..\..\jobaly\ontology\web_dev.owl"
+     ontology = OntologyLib(owlfile)
+     terms = [ " "+ x.lower() for x in ontology.getLabelList()]
+     terms.extend([" "+x.lower() for x in ontology.getAllClassNames()])
+     
+     srcBbClient = DbClient('localhost', 27017, "jobaly_daily_test")
+     newCol = srcBbClient.getCollection("daily_job_webdev")
+     
+     collection = newCol
+     
+     matchingSents = []
+     for job in collection.find(): 
+      #   print "\n\n\n======",job["_id"],"============================\n"
+        jobDesc = JobDescParser.parseJobDesc(job)
+        sents = jobDesc.listAllSentences() 
+        jid = job["_id"]
+        for sent in sents:
+            c = 0
+            sent = sent.lower()
+            for term in terms:                
+                if sent.find(term) != -1:
+                   c+=1
+                if c==3 : 
+                    print sent.encode("GBK", "ignore")
+                    matchingSents.append((jid, sent))
+                    break
+              
+     sortedsents = sorted(matchingSents, key=lambda x:   len(x[1]) )
+     dumpTwo(sortedsents, "..\skill\output\term" , ( lambda x: x[0] + ":" + x[1] ) )     
+    
+     
 def main(): 
  #   getJavaScipt()
-    testGetSentenceByTerm("html")
+  #  testGetSentenceByTerm("hadoop")
+    getSentsByOntology()
     
 if __name__ == "__main__": 
     main()   
