5be10241152fc24d796cb4ac2f783a1a2611313b
Frank Guo <pkushiqiang@gmail.com>
2014-10-05 17:05:09 -0500

    update

--- python/src/ir/baseir.py
+++ python/src/ir/baseir.py
@@ -4,8 +4,32 @@ Created on Sat Oct 04 22:44:19 2014
 
 @author: dlmu__000
 """
+from tfgetter import TfGetter
+import irutils
 
 class BaseIr():
+    
+    def __init__(self, jobCollection):
+         self.tfgetter =  TfGetter()   
+         self.jobCollection = jobCollection
+         self.processColl(self.jobCollection)  
+         
+    def processColl(self, jobcoll ):     
+         self.jobs = []        
+         self.doc_num = 0
+         sum_length = 0
+         for item in jobcoll.find(): 
+             content = irutils.processText(item["summary"])    
+             tokens =  self.tfgetter.getTokens(content)
+             tf = self.tfgetter.getTf(tokens)      
+   #          print "tf=",  tf
+             item['tf'] =  tf
+             item['length'] = len(tokens)
+             self.jobs.append(item)
+             self.doc_num+=1
+             sum_length += item['length']
+         self.avgLength = sum_length/self.doc_num
+         print "self.avgLength =", self.avgLength
     
     def matchResume(self, resume):
         self.calculateScores(resume)
--- python/src/ir/okapi.py
+++ python/src/ir/okapi.py
@@ -5,37 +5,18 @@ Created on Sat Oct 04 22:42:06 2014
 @author: dlmu__000
 """
 from baseir import BaseIr
-from tfgetter import TfGetter
 import irutils
+
 from jobaly.db.dbclient import DbClient 
 import math
 
 K1 = 1.2
 B  = 0.75
 
-class Okapi(BaseIr):
+class Okapi(BaseIr):   
     
-    def __init__(self, jobCollection):
-         self.tfgetter =  TfGetter()   
-         self.jobCollection = jobCollection
-         self.processColl(self.jobCollection)    
          
-    def processColl(self, jobcoll ):     
-         self.jobs = []        
-         self.doc_num = 0
-         sum_length = 0
-         for item in jobcoll.find(): 
-             content = irutils.processText(item["summary"])    
-             tokens =  self.tfgetter.getTokens(content)
-             tf = self.tfgetter.getTf(tokens)      
-   #          print "tf=",  tf
-             item['tf'] =  tf
-             item['length'] = len(tokens)
-             self.jobs.append(item)
-             self.doc_num+=1
-             sum_length += item['length']
-         self.avgLength = sum_length/self.doc_num
-         print "self.avgLength =", self.avgLength
+   
          
     def calculateScores(self,resume):
         resume_content = irutils.processText(resume) 
