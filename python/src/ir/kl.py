5b498f07caf8e922038454da560a541bce6bc63b
Frank Guo <pkushiqiang@gmail.com>
2014-10-05 17:20:52 -0500

    add kl

---
+++ python/src/ir/kl.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 05 13:40:34 2014
+
+@author: dlmu__000
+"""
+
+from baseir import BaseIr
+import irutils
+from jobaly.db.dbclient import DbClient 
+import math
+
+K1 = 1.2
+B  = 0.75
+
+class KL(BaseIr):    
+     
+         
+    def calculateScores(self,resume):
+        resume_content = irutils.processText(resume) 
+        resume_tokens =  self.tfgetter.getTokens(resume_content)
+        resumetf = self.tfgetter.getTf(resume_tokens) 
+        resume_len = len(resume_tokens)
+        resume_pq = {}
+        for key in resumetf.keys():
+            resume_pq[key] = float(resumetf[key])/resume_len
+        print "resume_pq=", resume_pq        
+       
+        for job in self.jobs:
+             tf = job["tf"]
+             score = 0
+             for key in resumetf.keys():
+                 if tf.has_key(key):
+                     score += resume_pq[key] * math.log10 (resume_pq[key] / ( float (tf[key]) / job["length"]) )  
+             job["score"] = score
+             
+def main(): 
+    #webJobInfoCollName: test_jobinfo
+
+    dbClient = DbClient('localhost', 27017, "jobaly")  
+    jobCollection = dbClient.getCollection("test_jobinfo")  
+    kl = KL(jobCollection)
+    resume = "I a am good java programmer, PHP, XML, hope juse c++, skill" 
+    jobs = kl.matchResume(resume)
+    
+    for job in jobs:
+        print job["_id"], job["score"]
+    
+if __name__ == "__main__": 
+    main()
